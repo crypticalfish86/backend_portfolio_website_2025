c98a05995fb2ce5f3e794b34366203888370bd88
crypticalfish86 <jasonweerawardena@gmail.com>
2025-01-22 02:56:21 +0000

    first version of app with just getters set up

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 /*
-    The app.use get called in order of the line number before every single api request
+    The app.use get called in order of the line number, any app.use written before your endpoints 
+    triggers before and every single app.use after your endpoints triggers after 
+    (which is why the unhandled error app.use is positioned afterwards)
 */
 
 const express = require('express');
@@ -14,3 +16,13 @@ app.get('/api', getEndpoints); //return all endpoints possible with this api
 app.get('/api/project_titles', getAllTitles); //return every single project title as an array (for displaying the project files with titles)
 
 app.get('/api/project_titles/:title', getProjectByID);//return all information (projects, project_details, images) about one project (activated when clicking a file)
+
+
+//any Unhandled error is caught by this at the end
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
+
+
+module.exports = app; //export the app to be used elsewhere
